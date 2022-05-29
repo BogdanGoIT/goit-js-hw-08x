4a239a9ed439cbfefdc2a57bdc4958051ad6ac3d
BogdanGoIT <92575228+BogdanGoIT@users.noreply.github.com>
2022-05-29 12:38:44 +0300

    add files simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,30 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
 console.log(galleryItems);
+
+
+const galleryEl = document.querySelector('.gallery');
+
+function galleryItem(galleryItems) {
+    
+    return galleryItems.map(({ description, original, preview }) => {
+        return `<a class="gallery__item" href="${original}">
+                    <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+                </a>`
+    }).join('');
+
+
+}
+
+const listPictures = galleryItem(galleryItems);
+
+galleryEl.innerHTML = listPictures;
+
+var lightbox = new SimpleLightbox('.gallery a ', { captionDelay: 250, });
